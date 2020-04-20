259f80287d844c725e1ac0865892375094cc2bea
Kalebe <62080789+Kalebeadv@users.noreply.github.com>
2020-04-20 15:51:19 -0300

    Update atletas.c

--- atletas.c
+++ atletas.c
@@ -4,7 +4,6 @@
 
 
 void determina_o_tempo_de_cada_participante( int classificacao[281][1]);
-//void determina_a_colocacao_dos_participantes(int participantes[], int tempo_dos_particiantes[]);
 
    
 int main()
@@ -13,7 +12,6 @@ int main()
   int classificacao[281][1];
   determina_o_tempo_de_cada_participante(classificacao);
   
- // determina_a_colocacao_dos_participantes(participantes,tempo_dos_participantes);
  
 }
 
@@ -30,13 +28,3 @@ void determina_o_tempo_de_cada_participante( int classificacao[281][1])
   }
 }
 
-/*void determina_a_colocacao_dos_participantes(int participantes[], int tempo_dos_particiantes[])
-{
- for(int i = 0; i < 280; i++)
- {
-   printf("posticao: [%d] tempo: %d\n", i,tempo_dos_particiantes[i]);
-   
-
- } 
-}
-*/
