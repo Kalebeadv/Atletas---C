5be12ecfadea69734cb42dce7d65bc4495dbfd95
Kalebe <62080789+Kalebeadv@users.noreply.github.com>
2020-04-20 15:49:37 -0300

    Update atletas.c

--- atletas.c
+++ atletas.c
@@ -1,39 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define tempo 400
-#define numero_de_participantes 280
 
-void determina_o_tempo_de_cada_participante( float tempo_dos_participantes[]);
-void determina_a_colocacao_dos_participantes(int participantes[], float tempo_dos_particiantes[]);
+
+void determina_o_tempo_de_cada_participante( int classificacao[281][1]);
+//void determina_a_colocacao_dos_participantes(int participantes[], int tempo_dos_particiantes[]);
 
    
 int main()
 {
-  int participantes[280];
-  float tempo_dos_participantes[280];
-  determina_o_tempo_de_cada_participante(tempo_dos_participantes);
+
+  int classificacao[281][1];
+  determina_o_tempo_de_cada_participante(classificacao);
   
-  determina_a_colocacao_dos_participantes(participantes,tempo_dos_participantes);
+ // determina_a_colocacao_dos_participantes(participantes,tempo_dos_participantes);
  
 }
 
-void determina_o_tempo_de_cada_participante( float tempo_dos_participantes[])
+void determina_o_tempo_de_cada_participante( int classificacao[281][1])
 {
-  float j;
-  for(int i = 0; i < 280; i++)
+  
+  for(int i = 1; i < 281; i++)
   {
-    j = i+1;
-    tempo_dos_participantes[i] = j;
+   for(int j = 0; j < 1; j++)
+   {
+     classificacao[i][j] =  i;
+     printf("posicao : [%d]   valor : %d\n", classificacao[i][j], i);
+   }
   }
 }
 
-void determina_a_colocacao_dos_participantes(int participantes[], float tempo_dos_particiantes[])
+/*void determina_a_colocacao_dos_participantes(int participantes[], int tempo_dos_particiantes[])
 {
  for(int i = 0; i < 280; i++)
  {
-   printf("posticao: [%d] tempo: %.2f\n", i,tempo_dos_particiantes[i]);
+   printf("posticao: [%d] tempo: %d\n", i,tempo_dos_particiantes[i]);
    
 
  } 
 }
+*/
