e789d5065c4149bd845953724ebc15630bd0ce21
Kalebe <62080789+Kalebeadv@users.noreply.github.com>
2020-04-20 17:06:57 -0300

    Update atletas.c

--- atletas.c
+++ atletas.c
@@ -8,6 +8,7 @@ void exibe_o_tempo_dos_tres_primeiros_colocados(int classificacao[281][1]);
 int exibe_a_media_dos_8_primeiros_colocados(int classificacao[281][1]);
 void exibe_o_percentual_de_atletas_ACIMA_da_media(int classificacao[281][1],int media);
 void exibe_o_percentual_de_atletas_ABAIXO_da_media(int classificacao[281][1],int media);
+void exibe_atletas_com_o_tempo_de_ate_135_minutos(int classificacao[281][1]);
 
    
 int main()
@@ -19,13 +20,13 @@ int main()
   float media = exibe_a_media_dos_8_primeiros_colocados(classificacao);
   exibe_o_percentual_de_atletas_ACIMA_da_media(classificacao,media);
   exibe_o_percentual_de_atletas_ABAIXO_da_media(classificacao,media);
+  exibe_atletas_com_o_tempo_de_ate_135_minutos(classificacao);
   
  
 }
 
 void determina_o_tempo_de_cada_participante( int classificacao[281][1])
 {
-
   for(int i = 1; i < 281; i++)
   {
    for(int j = 0; j < 1; j++)
@@ -73,7 +74,7 @@ int exibe_a_media_dos_8_primeiros_colocados(int classificacao[281][1])
 void exibe_o_percentual_de_atletas_ACIMA_da_media(int classificacao[281][1],int media)
 {
   printf("\n------------------------------\n");
-  printf("Atletas com o tempo ACIMA da média\n\n");
+  printf("Atletas com o tempo ACIMA da media\n\n");
   for(int i = 1; i < 281; i++)
   {
    for(int j = 0; j < 1; j++)
@@ -86,7 +87,7 @@ void exibe_o_percentual_de_atletas_ACIMA_da_media(int classificacao[281][1],int 
 void exibe_o_percentual_de_atletas_ABAIXO_da_media(int classificacao[281][1],int media)
 {
   printf("\n------------------------------\n");
-  printf("Atletas com o tempo ABAIXO da média\n\n");
+  printf("Atletas com o tempo ABAIXO da media\n\n");
   for(int i = 1; i < 281; i++)
   {
    for(int j = 0; j < 1; j++)
@@ -96,3 +97,22 @@ void exibe_o_percentual_de_atletas_ABAIXO_da_media(int classificacao[281][1],int
   }
   printf("\n------------------------------\n");
 }
+
+void exibe_atletas_com_o_tempo_de_ate_135_minutos(int classificacao[281][1])
+{
+  printf("\n------------------------------\n");
+  printf("Atletas que tiveram o tempo de classificacao para participar\n");
+  printf("da competicao de travessia da baia do marejo\n\n");
+   for(int i = 1; i < 281; i++)
+  {
+   for(int j = 0; j < 1; j++)
+   {
+    
+    if(classificacao[i][j] <= 135) printf("%d atleta, tempo: %d minutos\n", i, classificacao[i][j]); 
+  
+   }
+  }
+  printf("\n------------------------------\n");
+}
+
+void exibe_o_pior_tempo_da_competicao()
